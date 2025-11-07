52b4bb94228b2f48fa4d83ad3519d4c66d1dbee6
miittt5777 <mitp8511@gmail.com>
2025-11-07 21:22:20 +0530

    logging and

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,12 @@ import logging
 import os
 from datetime import datetime
 
-# Create logs directory if not already present
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
-# Create a timestamped log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-# Set up logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
@@ -19,4 +16,4 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     logging.info("Logging has started successfully.")
-    print(f"✅ Log file created at: {LOG_FILE_PATH}")
+    print(f" Log file created at: {LOG_FILE_PATH}")
